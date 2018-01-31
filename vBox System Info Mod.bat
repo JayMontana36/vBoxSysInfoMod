274c272f067718538ca7f40604db6c700214b224
Jasen Samuels <jasensamuels@gmail.com>
2018-01-31 17:16:57 -0500

    Code optimization and cleanup is pretty much complete at this point

--- vBox System Info Mod.bat
+++ vBox System Info Mod.bat
@@ -1,63 +1,57 @@
 @echo off
-TITLE VirtualBox VM System Information Modifier v5 Experimental
 :init
-echo Starting VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36...
+set title=vBoxSysInfoMod - VirtualBox VM System Information Modifier v5.0 Experimental Alpha by JayMontana36
+TITLE %title%
+:vBoxLocationInit
+echo Starting %title%...
 C:
-IF NOT EXIST "C:\Program Files\Oracle\Virtualbox" goto vBoxNotFound
+IF NOT EXIST "C:\Program Files\Oracle\Virtualbox" goto vBoxLocateFailed
 cd "C:\Program Files\Oracle\Virtualbox"
-IF NOT EXIST "VBoxManage.exe" goto vBoxNotFound
+IF NOT EXIST "VBoxManage.exe" goto vBoxLocateFailed
 
 :ModifyVM
 cls
-echo VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36
+echo %title%
 echo.
-set /p VMname="Name of the VirtualBox VM to modify: "
-set /p SYSven="System Vendor (Dell, ASUS, Lenovo, ASRock, MSI, etc) to assign: "
-set /p SYSprod="Vendor Product (Optiplex 745, Rog 8, H50-55, etc): "
-set /p SYSdate="BIOS/System Build Date (in M/D/YYYY or MM/DD/YYYY): "
-
-for /f "tokens=1 delims=firmware=" %F in ('"VBoxManage showvminfo %VMname% --machinereadable | findstr firmware"') do set _vmMode=%~F
+VBoxManage list vms
+echo.
+set /p VMname="Which vBox VM do you wish to modify? "
+@REM for /f "tokens=1 delims=*{" %%F in ('"VBoxManage list vms | findstr %VMname%"') do set _VMname=%%~F
+@REM Set _VMname=%_VMname:"=%
+@REM IF [%_VMname%] NEQ [%VMname%] goto ModifyVM
+set /p SYSven="New System Manufacturer? "
+set /p SYSprod="New System Model? "
+set /p SYSdate="New BIOS Date (in M/D/YYYY or MM/DD/YYYY)? "
+for /f "tokens=1 delims=firmware=" %%F in ('"VBoxManage showvminfo %VMname% --machinereadable | findstr firmware"') do set _vmMode=%%~F
 IF [%_vmMode%] EQU [BIOS] set fw=pcbios
 IF [%_vmMode%] EQU [EFI] set fw=efi
 
-
-echo.
-
-
-echo Does "%VMname%" run in BIOS mode or EFI mode? If you're unsure, enter BIOS.
-echo Warning! Enter EFI ONLY IF YOU'RE POSITIVE "%VMname%" runs in EFI mode.
-set /p VMmode="Enter either EFI or BIOS: "
-
-:ModifyVMprep
+:ModifyVMsummary
 cls
-echo VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36
+echo %title%
 echo.
-echo Ready to modify System Info for vBox VM "%VMname%" in "%VMmode%" mode.
-echo System info for VM "%VMname%" will be changed to "%SYSven% %SYSprod%"
-echo BIOS/System Build Date for VM "%VMname%" will be changed to "%SYSdate%"
+echo Summary:
+echo Ready to modify vBox VM "%VMname%" whenever you're ready.
+echo %_VMmode% Information will be changed to "%SYSven% %SYSprod%"
+echo %_VMmode% Date will be changed to "%SYSdate%"
 echo.
-echo Warning! Before continuing, please shut down any/all VirtualBox VMs!
-echo You have been warned!
+echo Warning: Before continuing, please shutdown any/all vBox VMs you care about;
+echo failure to do so may result in data loss or data corruption for running VMs.
 pause
 
-:ModifyVMstart
-echo Closing any and all VirtualBox VM Windows...
+:ModifyVMtaskkill
+echo Force closing any and all VirtualBox VM windows...
 taskkill /F /IM VirtualBox.exe
 taskkill /F /IM VBoxSVC.exe
+
+:ModifyVMproperties
+cls
+echo %title%
 echo.
-echo Ignore any errors above, they're completely normal when VirtualBox isn't running in the background.
-echo.
-echo Suppressing VM Indicators in TaskManager and others for VM "%VMname%"
+echo Suppressing VM Indications in TaskManager and other areas for "%VMname%"
 echo ...
 VBoxManage modifyvm "%VMname%" --paravirtprovider none
-goto ModifyVM%VMmode%
-
-:exit
-start VirtualBox.exe
-exit
-
-:ModifyVMsys
-echo Modifying System Information for vBox "%VMname%" in BIOS Mode.
+echo Applying System Information to vBox "%VMname%" in %_VMmode% Mode.
 echo ...
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystemVendor" "%SYSven%"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystemProduct" "%SYSprod%"
@@ -66,12 +60,12 @@ VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystem
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystemSKU" "string:%random%"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystemFamily" "<empty>"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiSystemUuid" "9852bf98-b83c-49db-a8de-182c42c7226b"
-echo Modifying BIOS Information for vBox "%VMname%" in %fw% Mode.
+echo Applying BIOS Information to vBox "%VMname%" in %_VMmode% Mode.
 echo ...
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBIOSVendor" "%SYSven%"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBIOSVersion" "string:%random%"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBIOSReleaseDate" "%SYSdate%"
-echo Modifying BaseBoard Information for vBox "%VMname%" in %fw% Mode.
+echo Applying Motherboard Information to vBox "%VMname%" in %_VMmode% Mode.
 echo ...
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBoardVendor" "%SYSven%"
 VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBoardProduct" "%SYSprod%"
@@ -83,15 +77,22 @@ VBoxManage setextradata "%VMname%" "VBoxInternal/Devices/%fw%/0/Config/DmiBoardB
 echo.
 echo Complete!
 echo.
-echo Successfully modified vBox System Information for vBox VM "%VMname%" in %fw% Mode!
+echo Successfully applied vBox System Information to vBox VM "%VMname%" in %_VMmode% Mode!
+echo.
+start VirtualBox.exe
+VBoxManage startvm %vmname% --type gui
 pause
 goto end
 
-:vBoxNotFound
+:exit
+start VirtualBox.exe
+exit
+
+:vBoxLocateFailed
 cls
-echo Failed to start VirtualBox VM System Information Changer by JayMontana36:
-echo VirtualBox was not found on this system in the default install location.
-echo Please install/reinstall VirtualBox in the default install location, or create a symlink/shortcut from the default install location.
+echo Failed to start %title%:
+echo VirtualBox was not found on this system in the default install location (Manually selecting it's location is not a thing yet).
+echo Please install/reinstall VirtualBox in the default install location, or create a symlink/shortcut in the default install location.
 pause
 exit
 
