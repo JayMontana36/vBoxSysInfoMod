1bc7f63de30191dd9834316669e8e365b10bd525
Jasen Samuels <jasensamuels@gmail.com>
2018-01-31 07:04:37 -0500

    Basic - Version Information Modification
    Simple for starters, changing version information to indicate a new experimental version in development; what I plan to do here is minimize, shrink, and optimize the code making it less complex, more simple to handle and manage, more dumbed down and straightforward. And eventually afterward, once this version is perfected and finalized, I will attempt a new GUI version of ths program/script based on/off this code since it'd just be easier to do so compared to now. Doing it now means I'd have to create the GUI version dependant upon the current version, create this version, then rewrite/update the GUI version to match.

--- vBox System Info Mod.bat
+++ vBox System Info Mod.bat
@@ -1,7 +1,7 @@
 @echo off
-TITLE VirtualBox VM System Information Modifier v4.0.1 Final
+TITLE VirtualBox VM System Information Modifier v5 Experimental
 :init
-echo Starting VirtualBox VM System Information Modifier v4.0.1 by JayMontana36...
+echo Starting VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36...
 C:
 IF NOT EXIST "C:\Program Files\Oracle\Virtualbox" goto vBoxNotFound
 cd "C:\Program Files\Oracle\Virtualbox"
@@ -9,7 +9,7 @@ IF NOT EXIST "VBoxManage.exe" goto vBoxNotFound
 
 :ModifyVM
 cls
-echo VirtualBox VM System Information Modifier v4.0.1 by JayMontana36
+echo VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36
 echo.
 set /p vmID="Name of the VirtualBox VM to modify: "
 set /p sysven="System Vendor (Dell, ASUS, Lenovo, ASRock, MSI, etc) to assign: "
@@ -22,7 +22,7 @@ set /p mode="Enter either EFI or BIOS: "
 
 :ModifyVMprep
 cls
-echo VirtualBox VM System Information Modifier v4.0.1 by JayMontana36
+echo VirtualBox VM System Information Modifier v5.0 Experimental by JayMontana36
 echo.
 echo Ready to modify System Info for vBox VM "%vmID%" in "%mode%" mode.
 echo System info for VM "%vmID%" will be changed to "%sysven% %sysprod%"
